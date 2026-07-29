7a7bcf869440c0739d2b5f183283957fd662ba0a
Prasuna Dahal <prasunadahal909@gmail.com>
2026-07-29 16:46:12 +0545

    SUPA-01:working in progress

--- lib/admin_screen/admin_dashboard.dart
+++ lib/admin_screen/admin_dashboard.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:task_app/providers/admin_provider/admin_dashboard_provider.dart';
+import 'package:task_app/resources/colors.dart';
+import '../Routes/Admin_Routes/admin_route.dart';
+import '../user_screen/profile_screen.dart';
+import 'admin_profile.dart';
 
 class AdminDashboard extends StatefulWidget {
   const AdminDashboard({super.key});
@@ -8,8 +14,340 @@ class AdminDashboard extends StatefulWidget {
 }
 
 class _AdminDashboardState extends State<AdminDashboard> {
+  late final AdminDashboardProvider provider = Provider.of<AdminDashboardProvider>(context,listen: false);
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        backgroundColor: Color(0xffF7F5EE),
+    body:CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          pinned: true,
+          // backgroundColor:CustomColors.appbar(context),
+            backgroundColor: const Color(0xffF7F5EE),
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          toolbarHeight:90,
+          title: Row(
+            children: [
+             GestureDetector(
+               onTap:(){
+                 // Navigator.push(
+                 //   context,
+                 //   MaterialPageRoute(builder: (context) => AdminProfile()),
+                 // );
+                 Navigator.pushNamed(context, Routes.adminProfileScreen);
+               },
+               child: CircleAvatar(
+                           radius: 24,
+                           backgroundImage:
+                           NetworkImage("https://img.magnific.com/premium-vector/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-vector-illustration_561158-3467.jpg?semt=ais_hybrid&w=740&q=80"),
+                         ),
+             ),
+           SizedBox(width: 12),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:[
+              Text(
+                "Hello,Admin",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 14,
+                ),
+              ),
+              SizedBox(height: 2),
+              Text(
+                "Prasuna Dahal",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+         ]
+        ),
+            actions:[
+              IconButton(onPressed: (){
+                provider.toggleTheme();
+              }, icon:Icon(
+                  provider.isDarkMode? Icons.light_mode:Icons.dark_mode,
+              )),
+              SizedBox(width: 15),
+              Icon(Icons.notifications_none),
+              SizedBox(width:20),
+            ]
+       ),
+        SliverToBoxAdapter(
+          child: SizedBox(height:5),
+        ),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding:  EdgeInsets.symmetric(horizontal:16),
+            child: Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(18),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children:  [
+                  Text(
+                    "Total Employee",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  SizedBox(height:5),
+                  Text(
+                    "20",
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff4B5D1A),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding:  EdgeInsets.symmetric(horizontal:12),
+            child: GridView.builder(
+              shrinkWrap: true,
+              physics:  NeverScrollableScrollPhysics(),
+              itemCount: provider.overview.length,
+              gridDelegate:
+               SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 12,
+                mainAxisSpacing: 12,
+                childAspectRatio: 1.5,
+              ),
+              itemBuilder: (context, index) {
+                return Container(
+                  height: MediaQuery.of(context).size.height*0.9,
+                  padding:  EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Column(
+                    crossAxisAlignment:
+                    CrossAxisAlignment.start,
+                    mainAxisAlignment:
+                    MainAxisAlignment.center,
+                    children: [
+                      Icon(provider.overview[index]['icon']),
+                      SizedBox(height:3),
+                      Text(
+                      '${provider.overview[index]["count"]}-${provider.overview[index]["title"]}',
+                        style:  TextStyle(
+                          fontSize:20,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xff4B5D1A),
+                        ),
+                      ),
+                       SizedBox(height:3),
+                      Text(
+                        provider.overview[index]["subtitle"],
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding:  EdgeInsets.symmetric(horizontal:12),
+            child: GridView.builder(
+              shrinkWrap: true,
+              physics:  NeverScrollableScrollPhysics(),
+              itemCount: provider.overview1.length,
+              gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 12,
+                mainAxisSpacing: 12,
+                childAspectRatio: 1.5,
+              ),
+              itemBuilder: (context, index) {
+                return Container(
+                  height: MediaQuery.of(context).size.height*0.9,
+                  padding:  EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Column(
+                    crossAxisAlignment:
+                    CrossAxisAlignment.start,
+                    mainAxisAlignment:
+                    MainAxisAlignment.center,
+                    children: [
+                      Icon(provider.overview1[index]['icon']),
+                      SizedBox(height:3),
+                      Text(
+                        '${provider.overview1[index]["count"]}-${provider.overview1[index]["title"]}',
+                        style:  TextStyle(
+                          fontSize:20,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xff4B5D1A),
+                        ),
+                      ),
+                      SizedBox(height:3),
+                      Text(
+                        provider.overview1[index]["subtitle"],
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+         SliverToBoxAdapter(
+          child: SizedBox(height: 22),
+        ),
+         SliverToBoxAdapter(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 18),
+            child: Text(
+              "All Assigned Tasks",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 22,
+              ),
+            ),
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding:  EdgeInsets.symmetric(horizontal: 18),
+            child: ListView.builder(
+              itemCount: provider.projectList.length,
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                final project = provider.projectList[index];
+                return Container(
+                  margin:  EdgeInsets.only(bottom: 14),
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                    BorderRadius.circular(18),
+                  ),
+                  child: Column(
+                    crossAxisAlignment:
+                    CrossAxisAlignment.start,
+                    children: [
+
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              project["title"],
+                              style:  TextStyle(
+                                fontWeight:
+                                FontWeight.w600,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+
+                          Container(
+                            padding:
+                             EdgeInsets.symmetric(
+                                horizontal: 10,
+                                vertical: 4),
+                            decoration: BoxDecoration(
+                              color: Colors.green.shade100,
+                              borderRadius:
+                              BorderRadius.circular(
+                                  20),
+                            ),
+                            child: Text(
+                              project["priority"],
+                              style: TextStyle(
+                                fontSize: 11,
+                                color: Colors.green,
+                                fontWeight:
+                                FontWeight.bold,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+
+                      SizedBox(height: 14),
+
+                      Row(
+                        children: [
+
+                           CircleAvatar(
+                            radius: 13,
+                            backgroundImage:
+                            NetworkImage(
+                                "https://img.magnific.com/premium-vector/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-vector-illustration_561158-3467.jpg?semt=ais_hybrid&w=740&q=80"),
+                          ),
+                           SizedBox(width: 8),
+                          Text(project["name"]),
+                          Spacer(),
+                          Container(
+                            padding:
+                             EdgeInsets.symmetric(
+                                horizontal: 12,
+                                vertical: 4),
+                            decoration: BoxDecoration(
+                              color: Colors.green.shade50,
+                              borderRadius:
+                              BorderRadius.circular(
+                                  20),
+                            ),
+                            child: Text(
+                              project["status"],
+                              style:  TextStyle(
+                                fontSize: 11,
+                              ),
+                            ),
+                          ),
+                           SizedBox(width: 10),
+                          Text(
+                            project["date"],
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 12,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ],
+    )
+    );
   }
 }
---
+++ lib/providers/admin_provider/admin_dashboard_provider.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class AdminDashboardProvider extends ChangeNotifier{
+  bool _isDarkMode = false;
+  bool get isDarkMode => _isDarkMode;
+  void toggleTheme() {
+    _isDarkMode = !_isDarkMode;
+    notifyListeners();
+  }
+
+  final List<Map<String, dynamic>> projectList = [
+    {
+      "title": "Task Management App",
+      "name": "Prasuna Dahal",
+      "status": "In Progress",
+      "priority": "HIGH",
+      "date": "Due 29 Aug, 2026",
+    },
+    {
+      "title": "GitHub Profile Viewer",
+      "name": "Jyoti Mandal",
+      "status": "In Progress",
+      "priority": "High",
+      "date": "Due 29 Aug, 2026",
+    },
+  ];
+
+  final List<Map<String,dynamic>> overview=[
+    {
+      "icon":Icons.task,
+      "count":"10",
+      "title":"Tasks",
+      "subtitle":"Total"
+
+    },
+    {
+      "icon":Icons.check_circle_outline,
+      "count":"5",
+      "title":"Tasks",
+      "subtitle":"Completed"
+    }
+  ];
+  final List<Map<String,dynamic>> overview1=[
+    {
+      "icon":Icons.hourglass_bottom,
+      "count":"10",
+      "title":"Tasks",
+      "subtitle":"Pending"
+
+    },
+    {
+      "icon":Icons.cancel,
+      "count":"5",
+      "title":"Tasks",
+      "subtitle":"Canceled"
+    }
+  ];
+}
